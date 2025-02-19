01e866ac71031fb0f910daa7cf6f9e31e0d40154
beamaia <beamaia.work@gmail.com>
2025-02-19 00:01:35 -0300

    validate params

--- src/validate_input/params.py
+++ src/validate_input/params.py
@@ -1,55 +1,138 @@
-from pydantic import BaseModel, Field, field_validator
+import pathlib as pl
 import warnings
-# project: # expected options [oscc_notoscc, dys_notdys, oscc_dys, multiclass]
-# run_name: ""
 
-# stages:
-#  - train: true
-#  - test: true
+from enum import Enum
+from aenum import MultiValueEnum
+from datetime import datetime
+from pydantic import BaseModel, Field, model_validator
 
-# dataset:
-#  - root: data
-#  - patch: patch_images
-#  - origin: original_images
+class ProjectEnum(str, Enum):
+    oscc_bin = "oscc_bin"
+    dys_bin = "dys_bin"
+    oscc_dys = "oscc_dys"
+    multiclass = "multiclass"
 
-# hyperparameters:
-#  - optimizer:
-#    - name: sgd
-#    - learning_rate: 0.001
-#  - scheduler:
-#    - name: reduce_lr_on_plateau
-#    - other:
-#       - factor: 0.1
-#       - patience: 5
-#       - min_lr: 0.000001
-#  - model:
-#    - name:
-#    - other:
-#  - other:
-#    - loss: cross_entropy
-#    - loss_weights: true
-#    - folds: 5
-#    - epochs: 200
-#    - batch_size: 30
+class OptimizerEnum(str, MultiValueEnum):
+    sgd = "sgd" , "SGD"
+    adam = "Adam", "adam"
+
+class SchedulerEnum(str, Enum):
+    reduce_lr_on_plateau = "reduce_lr_on_plateau"
+
+class ModelEnum(str, Enum):
+    resnet50 = "resnet50"
+    mobilenet = "mobilenet"
+    densenet121 = "densenet121"
+    vgg16 = "vgg16"
+    coat_lite_small = "coat_lite_small"
+    pit_s_distilled_224 = "pit_s_distilled_224"
+    vit_small_patch_384 = "vit_small_patch16_384"
+
+class Optimizer(BaseModel):
+    name: OptimizerEnum = Field(OptimizerEnum.sgd, alias="name")
+    learning_rate: float = Field(0.001, alias="learning_rate")
+
+class Scheduler(BaseModel):
+    name: SchedulerEnum = Field(SchedulerEnum.reduce_lr_on_plateau, alias="name")
+    other: dict | None = Field(None, alias="other")
+
+class Model(BaseModel):
+    name: ModelEnum = Field(alias="name")
+    other: dict | None = Field(None, alias="other")
+
+class OtherHyperparams(BaseModel):
+    loss: str = Field(alias="loss")
+    loss_weights: bool  = Field(True, alias="loss_weights")
+    folds: int = Field(5, alias="folds", min=1)
+    epochs: int = Field(200, alias="epochs", min=1)
+    batch_size: int = Field(30, alias="batch_size", min=2)
 
 class Hyperparameters(BaseModel):
-    pass
+    optimizer: Optimizer = Field(alias="optimizer")
+    scheduler: Scheduler = Field(alias="scheduler")
+    model: Model = Field(alias="model")
+    other: OtherHyperparams = Field(alias="other")
 
 class Dataset(BaseModel):
-    pass
+    root: str = Field("data", alias="data")
+    patch: str = Field("patch_images", alias="patch_images")
+    origin: str = Field("original_images", alias="original_images")
 
 class Stages(BaseModel):
-    pass
-
+    train: bool = Field(True, alias="train")
+    test: bool = Field(True, alias="test")
+    
 class Parameters(BaseModel):
-    project: str = Field(None, )
-    run_name: str = Field()
-    stages: Stages = Field(None, "stages", )
-    dataset: Dataset = Field(None, "dataset")
-    hyperparameters: Hyperparameters = Field(None, "hyperparameters")
+    project: ProjectEnum = Field(alias="project")
+    stages: Stages = Field(alias="stages")
+    dataset: Dataset = Field(alias="dataset")
+    hyperparameters: Hyperparameters = Field(alias="hyperparameters")
+    run_name: str = Field(alias="run_name")
 
-    @field_validator()
-    def run_name_not_empty(cls, v):
-        if not len(v):
-            warnings.warn(f"Empty run name, changing it to ", UserWarning)
-        pass
+    @model_validator(mode="after")
+    def run_name_not_empty(self):
+        run_name = self.run_name
+        model_name = self.hyperparameters.model.name.value
+        optimizer_name = self.hyperparameters.optimizer.name.value
+        if not len(run_name):
+            new_run_name  = model_name + "_" +\
+                   optimizer_name + "_" +\
+                   datetime.now().strftime("%Y_%m_%d_%H_%M_%S%Z")
+            
+            warnings.warn(f"Empty run name, will be set {new_run_name}", UserWarning)
+            self.run_name  = new_run_name
+        return self
+    
+    def __str__(self):
+        dashes = "-"*90
+        message = f"""{dashes}
+* Project: {self.project.value} - Run: {self.run_name}
+{dashes}
+* Stages to execute:
+"""
+        stages_to_execute = "\n".join(["- "+ stage[0] for stage in self.stages if stage[1]])
+        message = message + stages_to_execute
+
+        root_path = pl.Path(self.dataset.root)
+        patch_path = pl.Path(root_path / self.dataset.patch)
+        origin_path = pl.Path(root_path / self.dataset.origin)
+
+        message = message + f"""
+{dashes} 
+* Dataset path:
+patch: {patch_path}
+origin: {origin_path}
+{dashes}
+* Hyperparams:
+{dashes}
+** Model:
+name: {self.hyperparameters.model.name.value}
+"""
+        other_model = self.hyperparameters.model.other
+        if other_model != None or (isinstance(other_model, dict) and other_model):
+            other_model_str = "\n".join([f"{key}: {item}" for key, item in other_model.items()])
+            message = message + other_model_str
+
+        message = message + f"""
+{dashes}
+** Optimizer:
+name: {self.hyperparameters.optimizer.name.value}
+learning_rate: {self.hyperparameters.optimizer.learning_rate}
+{dashes}
+** Scheduler:
+name: {self.hyperparameters.scheduler.name.value}
+"""
+        other_scheduler = self.hyperparameters.scheduler.other
+        if other_scheduler != None or (isinstance(other_scheduler, dict) and other_scheduler):
+            other_scheduler_str = "\n".join([f"{key}: {item}" for key, item in other_scheduler.items()])
+            message = message + other_scheduler_str
+
+        message = message + f"""
+{dashes}
+** Other:
+loss: {self.hyperparameters.other.loss}
+weighted_loss: {self.hyperparameters.other.loss_weights}
+folds: {self.hyperparameters.other.folds}
+epochs: {self.hyperparameters.other.epochs}
+batch_size: {self.hyperparameters.other.batch_size}"""
+        return message
