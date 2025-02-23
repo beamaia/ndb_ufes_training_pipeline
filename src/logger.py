ce8f99b80444d6e71fba2b8d2f123822ad467397
beamaia <beamaia.work@gmail.com>
2025-02-23 13:21:18 -0300

    create logger

---
+++ src/logger.py
@@ -0,0 +1,41 @@
+import sys
+import logging
+import coloredlogs
+import os
+
+# add color to logs
+coloredlogs.install(level='INFO')
+
+logger = logging.getLogger(__name__)
+# create console handler and set level to info
+ch = logging.StreamHandler(sys.stdout)
+log_file = "logs/log.txt"
+
+if not os.path.exists("logs"):
+    os.mkdir("logs")
+
+if os.path.exists(log_file):
+    file_exists = True
+    suffix = 1
+
+    while file_exists:
+        new_file = f"logs/log_{str(suffix).zfill(2)}.txt"
+        if os.path.exists(new_file):
+            suffix += 1
+            continue
+        break
+    
+    log_file = new_file
+fileh = logging.FileHandler(log_file)
+
+# create formatter
+formatter = logging.Formatter(
+    "%(asctime)s %(levelname)s: %(message)s", "%Y-%m-%d %H:%M:%S"
+)
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+logger.addHandler(fileh)
