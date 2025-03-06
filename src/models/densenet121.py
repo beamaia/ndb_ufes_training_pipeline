44cf730835c1e4fe32d6fc688267d58ab2545566
beamaia <beamaia.work@gmail.com>
2025-03-06 00:15:53 -0300

    refactor DenseNet121 model initialization and forward method for improved structure

--- src/models/densenet121.py
+++ src/models/densenet121.py
@@ -8,16 +8,17 @@ from torchvision.models.densenet import densenet121
 class DenseNet121(nn.Module):
     def __init__(self, weights=models.DenseNet121_Weights.IMAGENET1K_V1, num_classes=3):
         super(DenseNet121, self).__init__()
-        densenet = densenet121(weights=weights)
-        self.features = densenet.features
-        self.classifier = nn.Linear(1024, num_classes) # the number of filters to learn in the first convolution layer -> number of classes
+        self.model = densenet121(weights=weights)
+        self.features = self.model.features
+        self.model.classifier = nn.Linear(self.model.classifier.in_features, num_classes) # the number of filters to learn in the first convolution layer -> number of classes
     
     def forward(self, x):
-        features = self.features(x)
+        # features = self.features(x)
         
-        out = F.relu(features, inplace=True)
-        out = F.adaptive_avg_pool2d(out, (1, 1))
-        out = flatten(out, 1)
-        out = self.classifier(out)
+        # out = F.relu(features, inplace=True)
+        # out = F.adaptive_avg_pool2d(out, (1, 1))
+        # out = flatten(out, 1)
+        # out = self.classifier(out)
+        out = self.model(x)
 
         return out
