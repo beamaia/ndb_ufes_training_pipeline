b5bf9c9e9e3f53df46bc05ec62090fc4853211cf
beamaia <beamaia.work@gmail.com>
2025-08-02 23:33:57 -0300

    add experiment script

---
+++ run_experiments.sh
@@ -0,0 +1,31 @@
+# ---------- oscc_bin ----------
+# mobilenetv2
+
+# densenet121
+python3 main.py
+
+# resnet50
+sed -i '' 's/name: densenet121/name: resnet50/' params.yaml
+python3 main.py
+
+# vgg16
+sed -i '' 's/name: resnet50/name: vgg16/' params.yaml
+python3 main.py
+
+# ---------- dys_bin ----------
+sed -i '' 's/project: oscc_bin/project: dys_bin/' params.yaml
+
+# vgg16
+python3 main.py
+
+# resnet50
+sed -i '' 's/name: vgg16/name: resnet50/' params.yaml
+python3 main.py
+
+# densenet121
+sed -i '' 's/name: resnet50/name: densenet121/' params.yaml
+python3 main.py
+
+# mobilenetv2
+sed -i '' 's/name: densenet121/name: mobilenetv2/' params.yaml
+python3 main.py
